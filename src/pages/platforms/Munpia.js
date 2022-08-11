5f5934ff451210af5514b7d7953944c1a30e0908
cxhuy <alvinkang1@gmail.com>
2022-08-12 04:30:38 +0900

    finish uploadperiodtable

--- src/pages/platforms/Munpia.js
+++ src/pages/platforms/Munpia.js
@@ -3,7 +3,7 @@ import PlatformInfo from "../../components/platform/PlatformInfo";
 import Heatmap from '../../components/platform/Heatmap';
 import GenreTable from '../../components/platform/GenreTable';
 import MonopolyTable from '../../components/platform/MonopolyTable';
-// import UploadPeriodTable from "../../components/platform/uploadPeriodTable.svelte";
+import UploadPeriodTable from '../../components/platform/UploadPeriodTable';
 // import PricingTable from "../../components/platform/pricingTable.svelte";
 // import KeywordsTagsTable from "../../components/platform/keywordsTagsTable.svelte";
 
@@ -16,7 +16,7 @@ function Munpia() {
         <Heatmap heatmapData={MunpiaData.heatmapData} platformGenres={MunpiaData.platformInfoData.platformGenres}/>
         <GenreTable genreData={MunpiaData.genreData}/>
         <MonopolyTable monopolyData={MunpiaData.monopolyData}/>
-        {/* <UploadPeriodTable weeklyUploadCountData={MunpiaData.weeklyUploadCountData}/> */}
+        <UploadPeriodTable uploadPeriodData={MunpiaData.weeklyUploadCountData}/>
         {/* <PricingTable pricingData={MunpiaData.pricingData}/> */}
         {/* <KeywordsTagsTable keywordsTagsData={MunpiaData.keywordsTagsData}/> */}
     </>
