8273351745b2454f08602d03e4a46df4034dceb4
cxhuy <alvinkang1@gmail.com>
2022-08-12 01:29:33 +0900

    add munpia page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Munpia from './pages/platforms/Munpia';
 import NotFound from './pages/NotFound';
 
 import Navbar from './components/global/navbar.js';
@@ -14,6 +15,7 @@ function App() {
                     <Navbar/>
                     <Routes>
                         <Route path='/' element={<Home/>}/>
+                        <Route path='/platforms/munpia' element={<Munpia/>}/>
                         <Route path='*' element={<NotFound/>}/>
                     </Routes>
                     <Footer/>
---
+++ src/pages/platforms/Munpia.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import PlatformInfo from "../../components/platform/platformInfo.svelte";
+// import Heatmap from "../../components/platform/heatmap.svelte"
+// import GenreTable from "../../components/platform/genreTable.svelte";
+// import MonopolyTable from "../../components/platform/monopolyTable.svelte";
+// import UploadPeriodTable from "../../components/platform/uploadPeriodTable.svelte";
+// import PricingTable from "../../components/platform/pricingTable.svelte";
+// import KeywordsTagsTable from "../../components/platform/keywordsTagsTable.svelte";
+
+import MunpiaData from "../../jsondata/munpia.json";
+
+function Munpia() {
+  return (
+    <div>Munpia</div>
+  )
+}
+
+export default Munpia
