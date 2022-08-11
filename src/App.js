6ec7c391858d5168aa1a5439c8a263e2f2853a58
cxhuy <alvinkang1@gmail.com>
2022-08-12 04:49:18 +0900

    add kakaopage page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Munpia from './pages/platforms/Munpia';
 import Novelpia from './pages/platforms/Novelpia';
+import Kakaopage from './pages/platforms/Kakaopage';
 import NotFound from './pages/NotFound';
 
 import Navbar from './components/global/navbar.js';
@@ -18,6 +19,7 @@ function App() {
                         <Route path='/' element={<Home/>}/>
                         <Route path='/platforms/munpia' element={<Munpia/>}/>
                         <Route path='/platforms/novelpia' element={<Novelpia/>}/>
+                        <Route path='/platforms/kakaopage' element={<Kakaopage/>}/>
                         <Route path='*' element={<NotFound/>}/>
                     </Routes>
                     <Footer/>
---
+++ src/pages/platforms/Kakaopage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Noticebar from '../../components/global/noticebar';
+import PlatformInfo from "../../components/platform/PlatformInfo";
+import Heatmap from '../../components/platform/Heatmap';
+import GenreTable from '../../components/platform/GenreTable';
+import MonopolyTable from '../../components/platform/MonopolyTable';
+import UploadPeriodTable from '../../components/platform/UploadPeriodTable';
+import KeywordsTagsTable from '../../components/platform/KeywordsTagsTable';
+
+import KakaopageData from "../../jsondata/kakaopage.json";
+
+function Kakaopage() {
+  return (
+    <>
+        <Noticebar noticeMsg="판드, BL 장르는 제외한 정보입니다."/>
+        <PlatformInfo platformInfoData={KakaopageData.platformInfoData}/>
+        <Heatmap heatmapData={KakaopageData.heatmapData} platformGenres={KakaopageData.platformInfoData.platformGenres}/>
+        <GenreTable genreData={KakaopageData.genreData}/>
+        <MonopolyTable monopolyData={KakaopageData.monopolyData}/>
+        <UploadPeriodTable uploadPeriodData={KakaopageData.weeklyUploadCountData}/>
+        <KeywordsTagsTable keywordsTagsData={KakaopageData.keywordsTagsData}/>
+    </>
+  )
+}
+
+export default Kakaopage
