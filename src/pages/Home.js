2f2dfe4c79e7db5a90acf5f4561bca2fcf435bd4
cxhuy <alvinkang1@gmail.com>
2022-08-12 20:12:44 +0900

    delete dark tag

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,5 @@
 import React, { useState, useEffect} from "react";
 
-// import MunpiaData from "../jsondata/munpia.json";
-// import NovelpiaData from "../jsondata/novelpia.json";
-// import KakaopageData from "../jsondata/kakaopage.json";
-// import KakaostageData from "../jsondata/kakaostage.json";
-// import NavernovelData from "../jsondata/navernovel.json";
-
 import MainPlatformInfo from "../components/platform/MainPlatformInfo";
 
 function Home() {
@@ -71,7 +65,7 @@ function Home() {
 
     return (
         <div className="container px-3 py-2.5 mt-6 flex flex-col">
-            <div className="mx-auto dark:text-gray-200 text-6xl">웹소지지</div>
+            <div className="mx-auto text-gray-200 text-6xl">웹소지지</div>
             <div className="mx-auto my-2 text-gray-600 dark:text-gray-400">
                 웹소지지는 최근 1주일 내에 연재 기록이 있는 작품들을 기준으로
                 데이터를 제공합니다.
