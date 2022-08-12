91b6c37ffcab6a955665011d310c5ed8282bc0c4
cxhuy <alvinkang1@gmail.com>
2022-08-12 19:18:23 +0900

    now fetches json instead of import

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,74 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 
-import MunpiaData from "../jsondata/munpia.json";
-import NovelpiaData from "../jsondata/novelpia.json";
-import KakaopageData from "../jsondata/kakaopage.json";
-import KakaostageData from "../jsondata/kakaostage.json";
-import NavernovelData from "../jsondata/navernovel.json";
+// import MunpiaData from "../jsondata/munpia.json";
+// import NovelpiaData from "../jsondata/novelpia.json";
+// import KakaopageData from "../jsondata/kakaopage.json";
+// import KakaostageData from "../jsondata/kakaostage.json";
+// import NavernovelData from "../jsondata/navernovel.json";
 
 import MainPlatformInfo from "../components/platform/MainPlatformInfo";
 
 function Home() {
+    const [MunpiaData, setMunpiaData] = useState(null);
+    const [NovelpiaData, setNovelpiaData] = useState(null);
+    const [KakaopageData, setKakaopageData] = useState(null);
+    const [KakaostageData, setKakaostageData] = useState(null);
+    const [NavernovelData, setNavernovelData] = useState(null);
+
+    const getData = async () => {
+        const response = await fetch("../jsondata/munpia.json", {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+        });
+        const data = await response.json();
+        setMunpiaData(data);
+
+        const response2 = await fetch("../jsondata/novelpia.json", {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+        });
+
+        const data2 = await response2.json();
+        setNovelpiaData(data2);
+
+        const response3 = await fetch("../jsondata/kakaopage.json", {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+        });
+        const data3 = await response3.json();
+        setKakaopageData(data3);
+
+        const response4 = await fetch("../jsondata/kakaostage.json", {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+        });
+        const data4 = await response4.json();
+        setKakaostageData(data4);
+
+        const response5 = await fetch("../jsondata/navernovel.json", {
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+        });
+        const data5 = await response5.json();
+        setNavernovelData(data5);
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    if (!MunpiaData || !NovelpiaData || !KakaopageData || !KakaostageData || !NavernovelData) return <div className="bg-gray-900 w-screen h-screen"></div>;
+
     return (
         <div className="container px-3 py-2.5 mt-6 flex flex-col">
             <div className="mx-auto dark:text-gray-200 text-6xl">웹소지지</div>
